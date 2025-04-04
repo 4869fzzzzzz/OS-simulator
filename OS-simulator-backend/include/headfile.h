bb15ef6b50646a86c30aefbbc3901c47435ac428
Zhao Fei <zf4869@bupt.edu.cn>
2025-04-04 20:23:27 +0800

    CREATE:backend

---
+++ OS-simulator-backend/include/headfile.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <algorithm>
+#include "./interupt.h"
+
+struct test{
+    int i;
+};
---
+++ OS-simulator-backend/src/main.cpp
@@ -0,0 +1,7 @@
+#include "../include/headfile.h"
+
+int main(){
+    struct test t;
+    t.i=10086;
+    std::cout<<"hello,OS"<<t.i<<std::endl;
+}
